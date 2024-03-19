1aef89ba835e0678aa49cc7a8f498b5833139941
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-03-18 23:58:30 -0400

    threading

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::{
     io::{BufRead, BufReader, Write},
     net::TcpListener,
+    thread,
 };
 
 use bytes::buf;
@@ -15,27 +16,29 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let bufreader = BufReader::new(stream.try_clone().unwrap());
-                bufreader.lines().for_each(|line| {
-                    let binding = line.unwrap_or("".to_owned());
-                    let line = binding.as_str();
-                    println!("{}", line);
-                    match line {
-                        "ping" => {
-                            let _ = &stream.write(b"+PONG\r\n").unwrap();
-                        }
-                        _ => {
-                            println!("err: {}", line);
-                        } // &stream.write(b"Unknown Command"),
-                    };
-                })
+        thread::spawn(move || {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
+                    let bufreader = BufReader::new(stream.try_clone().unwrap());
+                    bufreader.lines().for_each(|line| {
+                        let binding = line.unwrap_or("".to_owned());
+                        let line = binding.as_str();
+                        println!("{}", line);
+                        match line {
+                            "ping" => {
+                                let _ = &stream.write(b"+PONG\r\n").unwrap();
+                            }
+                            _ => {
+                                println!("err: {}", line);
+                            } // &stream.write(b"Unknown Command"),
+                        };
+                    })
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
