8e7886aaa8c14f76e1f32df0e1147c204e756973
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-03-19 20:14:52 -0400

    Upgraded input handling and set/get with expiry

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,16 @@ use std::{
     thread,
 };
 
+use chrono::{DateTime, Local, NaiveDateTime, Utc};
+
+const DEFAULT_DT: DateTime<Utc> = DateTime::from_timestamp_nanos(0);
+const NULL_REPLY: &[u8; 3] = b"_\r\n";
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let data_store = Arc::new(Mutex::new(HashMap::<String, String>::new()));
+    let data_store = Arc::new(Mutex::new(HashMap::<String, Data>::new()));
 
     // Uncomment this block to pass the first stage
 
@@ -29,6 +34,30 @@ fn main() {
                 println!("error: {}", e);
             }
         });
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Data {
+    value: String,
+    expires_at: DateTime<chrono::Utc>,
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        Data {
+            value: String::new(),
+            expires_at: chrono::Utc::now(),
+        }
+    }
+}
+
+impl Data {
+    pub fn new(data: String, expires: chrono::DateTime<chrono::Utc>) -> Self {
+        Data {
+            value: data,
+            expires_at: expires,
+        }
     }
 }
 
@@ -72,10 +101,14 @@ pub fn get_redis_type(input: &str) -> Option<RedisType> {
 pub fn read_input(
     buf: &mut BufReader<TcpStream>,
     stream: &mut TcpStream,
-    data_store: Arc<Mutex<HashMap<String, String>>>,
+    data_store: Arc<Mutex<HashMap<String, Data>>>,
 ) {
     let mut input_str = String::new();
-    let _ = buf.read_line(&mut input_str);
+    let test = buf.read_line(&mut input_str);
+
+    if let Err(test) = test {
+        println!("err: {}", test);
+    }
 
     let input_str = input_str.trim();
 
@@ -92,13 +125,15 @@ pub fn read_input(
         return;
     }
 
+    // println!("{}", &input_str);
+
     let input_type = input_type.unwrap();
 
     let mut inputs: Vec<String> = Vec::new();
 
     match input_type {
         RedisType::Array => {
-            // println!("{}", input_str);
+            println!("{}", input_str);
             let (_, len) = input_str.split_at(1);
             // println!("len: {}", len);
             let len = len.trim();
@@ -111,19 +146,27 @@ pub fn read_input(
                     let temp = get_redis_type(&buf_type).unwrap_or(RedisType::SimpleError);
                     match temp {
                         RedisType::BulkString => {
-                            let (_, len) = input_str.split_at(1);
+                            let (old, len) = buf_type.split_at(1);
                             let len = len.trim();
                             let len = len.parse::<usize>();
                             let mut temp = String::new();
                             let _ = buf.read_line(&mut temp).unwrap();
-                            // temp = temp.trim().to_owned();
+                            temp = temp.trim().to_owned();
                             if len.is_err() {
                                 let _ =
                                     stream.write(b"-ERR invalid input - no length\r\n").unwrap();
                                 return;
                             };
                             let len = len.unwrap();
-                            if len == temp.len() {
+                            println!(
+                                "{}{}\n{}\n{:?}: {}",
+                                old,
+                                len,
+                                temp.len(),
+                                temp.as_bytes(),
+                                temp
+                            );
+                            if len != temp.len() {
                                 let _ = stream
                                     .write(b"-ERR invalid input - incorrect length\r\n")
                                     .unwrap();
@@ -139,8 +182,8 @@ pub fn read_input(
                     }
                     i += 1;
                 }
-                let data_store = parse_inputs(inputs, stream, data_store);
-                read_input(buf, stream, data_store);
+                parse_inputs(inputs, stream, Arc::clone(&data_store));
+                read_input(buf, stream, Arc::clone(&data_store));
             } else {
                 let _ = stream.write(b"-ERR invalid input length\r\n").unwrap();
             }
@@ -156,11 +199,11 @@ pub fn read_input(
 pub fn parse_inputs(
     mut inputs: Vec<String>,
     stream: &mut TcpStream,
-    data_store: Arc<Mutex<HashMap<String, String>>>,
-) -> Arc<Mutex<HashMap<String, String>>> {
+    data_store: Arc<Mutex<HashMap<String, Data>>>,
+) {
     if inputs.is_empty() {
         let _ = stream.write(b"-ERR invalid commands\r\n");
-        return data_store;
+        return;
     }
     let binding = inputs.first().unwrap().to_ascii_lowercase();
     let command = binding.as_str().trim();
@@ -172,8 +215,8 @@ pub fn parse_inputs(
         }
         "echo" => {
             if args.first().is_none() {
-                let _ = &stream.write(b"_\r\n");
-                return data_store;
+                let _ = &stream.write(NULL_REPLY);
+                return;
             }
             let msg = args.first().unwrap();
             let data = format!("${}\r\n{}\r\n", msg.len(), msg);
@@ -183,25 +226,122 @@ pub fn parse_inputs(
         "set" => {
             if args.len() < 2 {
                 let _ = &stream.write(b"-ERR incorrect arguments");
-                return data_store;
+                return;
             }
             let mut store = data_store.lock().unwrap();
+            if args.len() > 2 {
+                let mut args = args.iter();
+                let key = args.next().unwrap().to_string();
+                let value = args.next().unwrap().to_string();
+                let flag_check: Vec<String> =
+                    args.clone().map(|x| x.to_ascii_lowercase()).collect();
+                let nxflag = flag_check.contains(&"nx".to_string());
+                let xxflag = flag_check.contains(&"xx".to_string());
+                let getflag = flag_check.contains(&"get".to_string());
+                let mut expires = false;
+                let mut keepttl = false;
+                let mut exflag = String::from("");
+
+                for x in args.by_ref() {
+                    let x = x.to_ascii_lowercase();
+                    if ["ex", "px", "exat", "pxat", "keepttl"].contains(&x.as_str()) {
+                        expires = true;
+                        if x == "keepttl" {
+                            keepttl = true;
+                        }
+                        exflag = x;
+                        break;
+                    }
+                }
+
+                if (store.contains_key(&key) && nxflag) || (!store.contains_key(&key) && xxflag) {
+                    let _ = &stream.write(NULL_REPLY);
+                    return;
+                }
+
+                let expiry: DateTime<Utc> = if expires && !keepttl {
+                    match exflag.as_str() {
+                        "ex" => DateTime::checked_add_signed(
+                            Utc::now(),
+                            chrono::TimeDelta::try_seconds(
+                                args.next().unwrap().parse::<i64>().unwrap(),
+                            )
+                            .unwrap_or_default(),
+                        )
+                        .unwrap(),
+                        "px" => DateTime::checked_add_signed(
+                            Utc::now(),
+                            chrono::TimeDelta::try_milliseconds(
+                                args.next().unwrap().parse::<i64>().unwrap(),
+                            )
+                            .unwrap_or_default(),
+                        )
+                        .unwrap(),
+                        "exat" => DateTime::from_timestamp_millis(
+                            args.next().unwrap().parse::<i64>().unwrap() * 1000,
+                        )
+                        .unwrap(),
+                        "pxat" => DateTime::from_timestamp_millis(
+                            args.next().unwrap().parse::<i64>().unwrap(),
+                        )
+                        .unwrap(),
+                        _ => {
+                            let _ = &stream.write(b"-ERR improper date flag");
+                            return;
+                        }
+                    }
+                } else if expires {
+                    store.get(&key).unwrap().expires_at
+                } else {
+                    DEFAULT_DT
+                };
+
+                println!(
+                    "nx: {}\nxx: {}\nget: {}\nexpires: {}",
+                    nxflag, xxflag, getflag, expiry
+                );
+
+                if nxflag && xxflag {
+                    let _ = &stream.write(NULL_REPLY);
+                    return;
+                }
+
+                let old = store.insert(key, Data::new(value, expiry));
+
+                if let Some(old) = old {
+                    if getflag {
+                        let _ = &stream
+                            .write(format!("${}\r\n{}\r\n", old.value.len(), old.value).as_bytes())
+                            .unwrap();
+                    }
+                } else if getflag {
+                    let _ = &stream.write(NULL_REPLY).unwrap();
+                    return;
+                }
+                let _ = &stream.write(b"+Ok\r\n").unwrap();
+                return;
+            }
             store.insert(
                 args.first().unwrap().to_string(),
-                args.get(1).unwrap().to_string(),
+                Data::new(args.get(1).unwrap().to_string(), DEFAULT_DT),
             );
             let _ = &stream.write(b"+OK\r\n").unwrap();
         }
         "get" => {
             if args.is_empty() {
                 let _ = &stream.write(b"-ERR incorrect arguments");
-                return data_store;
+                return;
             }
             let store = data_store.lock().unwrap();
-            let value = store.get(args.first().unwrap());
-            if let Some(value) = value {
-                let data = format!("${}\r\n{}\r\n", value.len(), value);
-                let _ = &stream.write(data.as_bytes());
+            let data = store.get(args.first().unwrap());
+            if let Some(data) = data {
+                let diff = data.expires_at.signed_duration_since(Utc::now());
+                if diff <= chrono::Duration::zero() {
+                    let _ = &stream.write(NULL_REPLY);
+                    return;
+                }
+                let output = format!("${}\r\n{}\r\n", data.value.len(), data.value);
+                let _ = &stream.write(output.as_bytes());
             } else {
                 let _ = &stream.write(b"$-1\r\n");
             }
@@ -211,5 +351,4 @@ pub fn parse_inputs(
             // let _ = &stream.write(b"-Unknown Command").unwrap();
         }
     };
-    data_store
 }
