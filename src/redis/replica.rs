a63255defc7964a8de5c56515f0742b676bafaff
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-03-20 14:32:23 -0400

    conf changes

--- src/redis/replica.rs
+++ src/redis/replica.rs
@@ -9,4 +9,18 @@ pub fn connect_to_master(replica: Replica) {
     let stream = binding.borrow_mut();
 
     let _ = &stream.write(b"*1\r\n$4\r\nping\r\n").unwrap();
+
+    let _ = &stream
+        .write(
+            format!(
+                "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${}\r\n{}\r\n",
+                replica.port.to_string().len(),
+                replica.port
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+    let _ = &stream
+        .write(b"*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
+        .unwrap();
 }
