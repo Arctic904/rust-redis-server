ca098c1bb7d1f59702cc3a74774c6de2feca6acd
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-03-20 14:13:39 -0400

    fix errors

--- src/redis/replica.rs
+++ src/redis/replica.rs
@@ -5,9 +5,8 @@ use crate::Replica;
 pub fn connect_to_master(replica: Replica) {
     //comment
     let connection = TcpStream::connect(format!("{}:{}", replica.host, replica.port));
-    let stream = connection
-        .expect("Misconfigured Master, invalid port or host")
-        .borrow_mut();
+    let mut binding = connection.expect("Misconfigured Master, invalid port or host");
+    let stream = binding.borrow_mut();
 
-    &stream.write(b"+PONG\r\n").unwrap();
+    let _ = &stream.write(b"+PONG\r\n").unwrap();
 }
