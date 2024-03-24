88989fff5ff5799eaef1349e6993c941a65bd446
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-03-24 02:19:23 -0400

    concurrency and context are always fun
    please change my mind

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ mod redis;
 
 // Uncomment this block to pass the first stage
 use std::{
+    cell::RefCell,
     collections::HashMap,
     io::BufReader,
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    rc::Rc,
     sync::{Arc, Mutex},
     thread,
 };
 
 use clap::Parser;
+use redis::replica;
 
 use crate::redis::handle_input::read_input;
 
@@ -18,6 +21,8 @@ pub struct Replica {
     host: String,
     port: u16,
 }
+
+pub type DataStore = Arc<Mutex<HashMap<String, redis::Data>>>;
 
 const NULL_REPLY: &[u8; 5] = b"$-1\r\n";
 const OK_REPLY: &[u8; 5] = b"+OK\r\n";
@@ -37,6 +42,22 @@ pub struct RedisServer {
         help = "Makes this server a replica of <HOST>:<PORT>",
     )]
     replicaof: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ReplicaConn {
+    host: String,
+    port: u16,
+    stream: Arc<Mutex<TcpStream>>,
+    buffer: Arc<Mutex<BufReader<TcpStream>>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Context {
+    bufreader: Rc<RefCell<BufReader<TcpStream>>>,
+    stream: Rc<RefCell<TcpStream>>,
+    store: DataStore,
+    replica_conn: Option<ReplicaConn>,
 }
 
 fn main() {
@@ -62,23 +83,39 @@ fn main() {
 
     let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).unwrap();
 
-    let (stream, buffer) = if let Some(replica) = replica.clone() {
+    let (rep_stream, rep_buffer) = if let Some(replica) = replica.clone() {
         redis::replica::connect_to_master(replica, args.port)
     } else {
         (None, None)
     };
 
+    let replica_conn = if let Some(replica) = replica {
+        Some(ReplicaConn {
+            host: replica.host,
+            port: replica.port,
+            stream: Arc::new(Mutex::new(rep_stream.unwrap())),
+            buffer: Arc::new(Mutex::new(rep_buffer.unwrap())),
+        })
+    } else {
+        None
+    };
+
     for stream in listener.incoming() {
-        let replica = replica.clone();
         let cloned_store = Arc::clone(&data_store);
+        let replica_conn = replica_conn.clone();
         let _test = thread::spawn(move || match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!(
                     "accepted new connection at {}",
                     stream.peer_addr().expect("Error")
                 );
-                let mut bufreader = BufReader::new(stream.try_clone().unwrap());
-                read_input(&mut bufreader, &mut stream, cloned_store, replica);
+                let bufreader = BufReader::new(stream.try_clone().unwrap());
+                read_input(Context {
+                    bufreader: Rc::new(RefCell::new(bufreader)),
+                    stream: Rc::new(RefCell::new(stream)),
+                    store: cloned_store,
+                    replica_conn,
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
--- src/redis/handle_input.rs
+++ src/redis/handle_input.rs
@@ -1,27 +1,19 @@
-use base64::prelude::*;
-use std::collections::HashMap;
-use std::io::{BufRead, BufReader, Write};
-use std::net::TcpStream;
-use std::sync::{Arc, Mutex};
-
-use chrono::Utc;
-
-use crate::NULL_REPLY;
-
 use super::decoder::CommandType::*;
 use super::decoder::InfoSelection::*;
 use super::set_parse::set_parse;
-use super::{get_redis_type, Data, RedisType};
-use crate::Replica;
+use super::{get_redis_type, RedisType};
+use crate::{Context, NULL_REPLY};
+use base64::prelude::*;
+use chrono::Utc;
+use std::borrow::BorrowMut;
+use std::io::{BufRead, Write};
 
-pub fn read_input(
-    buf: &mut BufReader<TcpStream>,
-    stream: &mut TcpStream,
-    data_store: Arc<Mutex<HashMap<String, Data>>>,
-    replica: Option<Replica>,
-) {
+pub fn read_input(mut context: Context) {
+    let mut bufreader = context.bufreader.borrow_mut().into_inner();
+    let mut stream = context.stream.borrow_mut().into_inner();
+
     let mut input_str = String::new();
-    let test = buf.read_line(&mut input_str);
+    let test = bufreader.read_line(&mut input_str);
 
     if let Err(test) = test {
         println!("err: {}", test);
@@ -59,7 +51,6 @@ pub fn read_input(
                 let mut i = 0;
                 while i < len {
                     let mut buf_type = String::new();
-                    let _ = buf.read_line(&mut buf_type).unwrap();
                     let temp = get_redis_type(&buf_type).unwrap_or(RedisType::SimpleError);
                     match temp {
                         RedisType::BulkString => {
@@ -67,7 +58,7 @@ pub fn read_input(
                             let len = len.trim();
                             let len = len.parse::<usize>();
                             let mut temp = String::new();
-                            let _ = buf.read_line(&mut temp).unwrap();
+                            let _ = bufreader.read_line(&mut temp).unwrap();
                             temp = temp.trim().to_owned();
                             if len.is_err() {
                                 let _ =
@@ -99,8 +90,9 @@ pub fn read_input(
                     }
                     i += 1;
                 }
-                parse_inputs(inputs, stream, Arc::clone(&data_store), replica.clone());
-                read_input(buf, stream, Arc::clone(&data_store), replica.clone());
+
+                parse_inputs(inputs, context.clone());
+                read_input(context.clone());
             } else {
                 let _ = stream.write(b"-ERR invalid input length\r\n").unwrap();
             }
@@ -113,12 +105,10 @@ pub fn read_input(
     let _ = stream.shutdown(std::net::Shutdown::Both);
 }
 
-pub fn parse_inputs(
-    inputs: Vec<String>,
-    stream: &mut TcpStream,
-    data_store: Arc<Mutex<HashMap<String, Data>>>,
-    replica: Option<Replica>,
-) {
+pub fn parse_inputs(inputs: Vec<String>, mut context: Context) {
+    let bufreader = context.bufreader.borrow_mut().into_inner();
+    let mut stream = context.stream.borrow_mut().into_inner();
+
     if inputs.is_empty() {
         let _ = stream.write(b"-ERR invalid commands\r\n");
         return;
@@ -145,9 +135,9 @@ pub fn parse_inputs(
             // println!("{}", data);
             let _ = &stream.write(data.as_bytes()).unwrap();
         }
-        Set(set) => set_parse(set, stream, data_store),
+        Set(set) => set_parse(set, &mut stream, context.store),
         Get(get) => {
-            let mut store = data_store.lock().unwrap();
+            let mut store = context.store.lock().unwrap();
             let data = store.get(&get.key);
             // println!("{:?}\n{}\n{:?}", store.keys(), get.key, data);
             if let Some(data) = data {
@@ -171,7 +161,7 @@ pub fn parse_inputs(
         }
         Info(info) => {
             //comment
-            let role = match replica {
+            let role = match context.replica_conn {
                 Some(_rep) => "slave".to_owned(),
                 None => "master".to_owned(),
             };
